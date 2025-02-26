25e6ffbb3f4fc032f7fb568da5987398d0b89a75
seokhyun <letushyun@gmail.com>
2025-02-26 16:35:29 +0900

    [Style] Envrionment 추가

---
+++ FreshNote/Resources/Environment.swift
@@ -0,0 +1,17 @@
+//
+//  Environment.swift
+//  FreshNote
+//
+//  Created by SeokHyun on 2/25/25.
+//
+
+import Foundation
+
+enum Environment {
+  // MARK: - Keys
+  enum Keys {
+    enum Plist: String {
+      case baseURL = "BASE_URL"
+    }
+  }
+}
